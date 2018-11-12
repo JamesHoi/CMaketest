fb94366361ddc014c1aa053fbdbbf1778dec4c4e
JamesHoi <james352053767@gmail.com>
2018-11-12 23:00:26 +0800

    Initial commit

---
+++ build/CMakeFiles/CMaketest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMaketest.dir/main.cpp.obj"
+  "CMaketest.pdb"
+  "CMaketest.exe"
+  "CMaketest.exe.manifest"
+  "libCMaketest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMaketest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/WorkSpace/C++/CMaketest
+# Build directory: D:/WorkSpace/C++/CMaketest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
